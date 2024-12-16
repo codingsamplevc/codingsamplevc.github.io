df949ff5a5b7d0a69426100f36dbc475bf0c13fa
codingsamplevc <150186422+codingsamplevc@users.noreply.github.com>
2024-12-16 16:52:16 +0100

    Create example_5.js

---
+++ example_5.js
@@ -0,0 +1,38 @@
+
+// test sudoku
+
+const test = {
+  len: 9,
+  board: null,
+  setupBoard: function (container_id){
+    // Array(test.len).fill(Array(test.len).fill(' '))
+    // Array(test.len).fill(Array(test.len).fill(' '))
+    // Array.from(Array(test.len), (a) => { Array.from(Array(test.len), (b) => ' ') })
+    const board = Array.from(
+      Array(test.len),
+      (a) => { Array.from(Array(test.len), (b) => ' ') });
+    test.board = board;
+
+    test.printBoard();
+
+    test.changeCell({ row: 0, col: 0 }, '5');
+    
+    test.printBoard();
+    
+    test.changeCell({ row: 8, col: 0 }, '3');
+    
+    test.printBoard();
+    
+    test.changeCell({ row: 8, col: 8 }, '7');
+    
+    test.printBoard();
+  },
+  printBoard: function (){
+    console.log(test.board);
+  },
+  changeCell: function (coords, val){
+    if (test.board){
+      test.board[coords.row][coords.col] = val;
+    }
+  },
+};
