c76ff89164a5b7c29723c7dd14a3b7b36982b97d
codingsamplevc <150186422+codingsamplevc@users.noreply.github.com>
2024-12-30 07:50:41 +0100

    Update example_5.js

--- example_5.js
+++ example_5.js
@@ -1,40 +1,100 @@
 
-// test sudoku
+// sudoku
 
-const test = {
+const sudoku = {
+  ids: {
+    container_id: 'js_5',
+    table: 'sudoku',
+    cell_prefix: 'sudo'
+  },
   len: 9,
   board: null,
   setupBoard: function (container_id){
-    // Array(test.len).fill(Array(test.len).fill(' '))
-    // Array(test.len).fill(Array(test.len).fill(' '))
-    // Array.from(Array(test.len), (a) => { Array.from(Array(test.len), (b) => ' ') })
+    // Array(sudoku.len).fill(Array(sudoku.len).fill(' '))
+    // Array(sudoku.len).fill(Array(sudoku.len).fill(' '))
+    // Array.from(Array(sudoku.len), (a) => { Array.from(Array(sudoku.len), (b) => ' ') })
     const board = Array.from(
-      Array(test.len),
+      Array(sudoku.len),
       (a) => a = Array.from(
-        Array(test.len),
+        Array(sudoku.len),
         (b) => b = ' ') );
-    test.board = board;
+    sudoku.board = board;
 
-    test.printBoard();
+    // sudoku.printBoard();
 
-    test.changeCell({ row: 0, col: 0 }, '5');
+    // sudoku.changeCell({ row: 0, col: 0 }, '5');
     
-    test.printBoard();
+    // sudoku.printBoard();
     
-    test.changeCell({ row: 8, col: 0 }, '3');
+    // sudoku.changeCell({ row: 8, col: 0 }, '3');
     
-    test.printBoard();
+    // sudoku.printBoard();
     
-    test.changeCell({ row: 8, col: 8 }, '7');
+    // sudoku.changeCell({ row: 8, col: 8 }, '7');
     
-    test.printBoard();
+    // sudoku.printBoard();
   },
   printBoard: function (){
-    console.log(JSON.stringify(test.board));
+    console.log(JSON.stringify(sudoku.board));
   },
   changeCell: function (coords, val){
-    if (test.board){
-      test.board[coords.row][coords.col] = val;
+    if (sudoku.board){
+      sudoku.board[coords.row][coords.col] = val;
+    }
+  },
+  makeTable: function (){
+  
+    function ret_cell(id){
+    const cell = document.createElement("td");
+    cell.id = id;
+    const inputfield = document.createElement("input");
+    inputfield.type = 'text';
+    inputfield.maxlength = 1;
+    inputfield.addEventListener('change', sudoku.updateListener);
+    cell.innerHTML = inputfield;
+    return cell;
+    }
+  
+    function ret_cellrow(i){
+      const row = document.createElement("tr");
+      for (let j = 0; j < sudoku.len; j++){
+        row.appendChild(ret_cell(`${sudoku.ids.cell_prefix}_${i}_${j}`));
+      }
+      return row;
+    }
+    
+    if (!document.getElementById(sudoku.ids.table)){
+      const table = document.createElement("table");
+      table.id = ttt.table_id;
+      
+      for (let i = 0; i < sudoku.len; i++){
+        const row = ret_cellrow(i);
+        table.appendChild(row);
+      }
+      
+      const container = document.getElementById(sudoku.ids.container_id);
+      container.appendChild(table);
+  
+      // const game_status_el = document.createElement("p");
+      // game_status_el.id = ttt.game_status_id;
+      // container.appendChild(game_status_el);
+      
+      // const create_button = document.getElementById(ttt.create_btn_id);
+      // create_button.setAttribute('onclick', 'ttt.reset()');
+      // create_button.innerHTML = "Reset board";
+    }
+  },
+  updateListener: function (e){
+    let coords = e.parentElement.id.split('_').filter((el) => !Number.isNaN(el));
+    sudoku.changeCell({ row: coords[0], col: coords[1] }, e.value);
+  },
+  updateTable: function (){
+    const values = sudoku.board;
+
+    for (let i = 0; i < sudoku.len; i++){
+      for (let j = 0; j < sudoku.len; j++){
+        sudoku.board[i][j]
+      }
     }
   },
 };
