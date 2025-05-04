446f23bcbabecd09fceae7547a5ea64ec02b4cbf
codingsamplevc <150186422+codingsamplevc@users.noreply.github.com>
2025-05-04 20:54:21 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -13,15 +13,28 @@ const test = {
   },
   createSVG_Circle: (size,xy,radius,color) => {
     const _svg = document.createElement('svg', { width: size, height: size });
-    _svg.width = size;
-    _svg.height = size;
-    const _circle = document.createElement('circle', { cx: xy, cy: xy, r: radius, style: `fill:${color}` });
-    _circle.cx = xy;
-    _circle.cy = xy;
-    _circle.r = radius;
-    _circle.style = `fill:${color}`;
-    _svg.append(_circle);
+    // _svg.width = size;
+    // _svg.height = size;
+    // const _circle = document.createElement('circle', { cx: xy, cy: xy, r: radius, style: `fill:${color}` });
+    // _circle.cx = xy;
+    // _circle.cy = xy;
+    // _circle.r = radius;
+    // _circle.style = `fill:${color}`;
+    // _svg.append(_circle);
+
+
+    
+    // Create the `style` element in the SVG namespace
+    const style = document.createElementNS("http://www.w3.org/2000/svg", "style");
+    const node = document.createTextNode("circle { fill: red; }"); // cx: 25, cy: 25, r: 12.5, 
+    style.appendChild(node);
+    
+    // Append the style element to the SVG element
+    _svg.appendChild(style);
+
+
     return _svg;
+
   },
   attach_SVG_toContainer: (element) => {
     const c = document.getElementById('svg_container');
