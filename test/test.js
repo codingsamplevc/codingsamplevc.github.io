acdb575b000f832af1c1f60cb14a0cd1f19b349d
codingsamplevc <150186422+codingsamplevc@users.noreply.github.com>
2025-05-04 18:28:57 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -5,15 +5,15 @@ const test = {
     },
     svg: {
       circle_size: 50,
-      circle_xy: this.constants.svg.circle_size / 2,
-      circle_r: this.constants.svg.circle_xy / 2,
-      circle_red: this.createSVG_Circle('red'),
-      circle_black: this.createSVG_Circle('black'),
+      circle_xy: test.constants.svg.circle_size / 2,
+      circle_r: test.constants.svg.circle_xy / 2,
+      circle_red: test.createSVG_Circle('red'),
+      circle_black: test.createSVG_Circle('black'),
     },
   },
   createSVG_Circle: (color) => {
-    const _svg = document.createElement('svg', { width: this.constants.svg.circle_size, height: this.constants.svg.circle_size });
-    const _circle = document.createElement('circle', { cx: this.constants.svg.circle_xy, cy: this.constants.svg.circle_xy, r: this.constants.svg.circle_r, style: `fill:${color}` });
+    const _svg = document.createElement('svg', { width: test.constants.svg.circle_size, height: test.constants.svg.circle_size });
+    const _circle = document.createElement('circle', { cx: test.constants.svg.circle_xy, cy: test.constants.svg.circle_xy, r: test.constants.svg.circle_r, style: `fill:${color}` });
     _svg.innerHTML = _circle;
     return _svg;
   },
