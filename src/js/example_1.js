d64714ab82a85cedb183cb1978dd51e8548434fa
codingsamplevc <150186422+codingsamplevc@users.noreply.github.com>
2025-10-15 11:47:30 +0200

    refactor: Made TicTacToe game into a class

--- src/js/example_1.js
+++ src/js/example_1.js
@@ -1,129 +1,144 @@
 // JavaScript example 1: TicTacToe
 
-const ttt = {
-  game_over: 0,
-  playerTurn: 1,
-  players: ['O', 'X'],
-  prefix: 'ttt',
-  table_id: 'tttTable',
-  create_btn_id: 'createTTT',
-  game_status_id: 'ttt_game_status',
-  get_Value: function (id){
-    return document.getElementById(id).innerHTML;
-  },
-  checkWinCondition: function (player){
-    let diagL = 0;
-    let diagR = 0;
-    for (let k = 0; k < 3; k++){
-      if (ttt.get_Value(`${ttt.prefix}_${k}_${k}`) == player){
-        diagL++;
+class TicTacToe {
+  #gameOver = false;
+  #turn = 0;
+  #players = ['O','X'];
+  #classNames = {
+    container: "tictactoe-container",
+    button: "tictactoe-btn",
+    table: "tictactoe-board",
+    status: "tictactoe-status",
+  };
+  #elements = {
+    container: null,
+    button: null,
+    table: null,
+    status: null,
+  };
+
+  constructor(container, button) {
+    this.#elements.container = container;
+    this.#elements.button = button;
+    this.#elements.button.addEventListener("click", () => this.#setupBoard());
+  }
+
+  #setupBoard() {
+    if (this.#elements.container) {
+      if (this.#elements.table) {
+        this.#reset();
+        return;
       }
-      if (ttt.get_Value(`${ttt.prefix}_${k}_${2 - k}`) == player){
-        diagR++;
-      }
-    }
-    
-    if (diagL == 3 || diagR == 3){
-      return true;
-    }
-    
-    let horiz = 0;
-    let vert = 0;
-    for (let i = 0; i < 3; i++){
-      for (let j = 0; j < 3; j++){
-        if (ttt.get_Value(`${ttt.prefix}_${i}_${j}`) == player){
-          horiz++;
-        }
-        if (ttt.get_Value(`${ttt.prefix}_${j}_${i}`) == player){
-          vert++;
-        }
-      }
+      const table = document.createElement("table");
+      table.classList.add(this.#classNames.table);
+      this.#elements.table = table;
+      this.#elements.container.appendChild(table);
       
-      if (horiz == 3 || vert == 3){
-        return true;
+      const status = document.createElement("p");
+      status.classList.add(this.#classNames.status);
+      this.#elements.status = status;
+      this.#elements.container.appendChild(status);
+
+      for (let i = 0; i < 3; ++i){
+        const row = this.#create_tableRow();
+        table.appendChild(row);
       }
-      horiz = 0;
-      vert = 0;
-    }
-    
-    return false;
-  },
-  get_CurrentPlayer: function (){
-    return ttt.players[ttt.playerTurn % 2];
-  },
-  advanceTurn: function (){
-    ttt.playerTurn++;
-  },
-  chooseCell: function (){
-    if (ttt.game_over) { return; }
-    const game_status_el = document.getElementById(ttt.game_status_id);
-    game_status_el.innerHTML = '';
-    if (this.innerHTML == ' '){
-      this.innerHTML = ttt.get_CurrentPlayer();
-      const curPlayer = ttt.get_CurrentPlayer();
-      if (ttt.playerTurn > 4 && ttt.checkWinCondition(curPlayer)){
-        game_status_el.innerHTML = `Player ${curPlayer} won!`;
-        ttt.game_over = 1;
-      }
-      ttt.advanceTurn();
-      if (ttt.playerTurn > 9) {
-        game_status_el.innerHTML = `It's a draw!`;
-        ttt.game_over = 1;
-      }
-    } else {
-      game_status_el.innerHTML = 'Cell has already been taken.';
-    }
-  },
-  reset: function (){
-    if (!document.getElementById(ttt.table_id)) { return; }
-    for (let i = 0; i < 3; i++){
-      for (let j = 0; j < 3; j++){
-        const cell = document.getElementById(`${ttt.prefix}_${i}_${j}`);
-        cell.innerHTML = ' ';
-      }
-    }
-    ttt.game_over = 0;
-    ttt.playerTurn = 1;
-    const game_status_el = document.getElementById(ttt.game_status_id);
-    game_status_el.innerHTML = '';
-  },
-  createBoard: function (container_id){
-      
-    function returnTableCell(id){
-      const tCell = document.createElement("td");
-      tCell.id = id;
-      tCell.onclick = ttt.chooseCell;
-      tCell.innerHTML = ' ';
-      return tCell;
-    }
-    
-    function returnTableRow(i){
-      const tRow = document.createElement("tr");
-      for (let j = 0; j < 3; j++){
-        tRow.appendChild(returnTableCell(`${ttt.prefix}_${i}_${j}`));
-      }
-      return tRow;
-    }
-    
-    if (!document.getElementById(ttt.table_id)){
-      const tableEl = document.createElement("table");
-      tableEl.id = ttt.table_id;
-      
-      for (let i = 0; i < 3; i++){
-        const tRow = returnTableRow(i);
-        tableEl.appendChild(tRow);
-      }
-      
-      const tableContainer = document.getElementById(container_id);
-      tableContainer.appendChild(tableEl);
-  
-      const game_status_el = document.createElement("p");
-      game_status_el.id = ttt.game_status_id;
-      tableContainer.appendChild(game_status_el);
-      
-      const create_button = document.getElementById(ttt.create_btn_id);
-      create_button.setAttribute('onclick', 'ttt.reset()');
-      create_button.innerHTML = "Reset board";
     }
   }
-};
+
+  #create_tableRow() {
+    const row = document.createElement("tr");
+    for (let i = 0; i < 3; ++i){
+      row.appendChild(this.#create_tableCell());
+    }
+    return row;
+  }
+
+  #create_tableCell() {
+    const cell = document.createElement("td");
+    cell.innerHTML = ' ';
+    cell.addEventListener("click", (ev) => {
+      if (this.#isClickable(ev.currentTarget)) {
+        ev.currentTarget.innerHTML = `${this.#players[this.#turn]}`;
+        if (this.#isGameWon()) {
+          this.#elements.status.innerHTML = `Player ${this.#players[this.#turn]} won!`;
+          this.#gameOver = true;
+          return;
+        }
+        this.#clearStatus();
+        this.#turn = this.#turn ? 0 : 1;
+      }
+    });
+    return cell;
+  }
+
+  #isClickable(element) {
+    if (this.#gameOver) { return false; }
+    if (!(element.innerHTML === ' ')) {
+      this.#elements.status.innerHTML = "Square already taken.";
+      return false;
+    }
+    return true;
+  }
+
+  #isGameWon() {
+    if (!this.#elements.table) { return; }
+    const rows = this.#elements.table.children;
+    if (!rows) { return; }
+    
+    let diagonalUpperleft = 0;
+    let diagonalUpperRight = 0;
+    for (let i = 0; i < 3; ++i) {
+      if (rows.item(i).children.item(i).innerHTML === `${this.#players[this.#turn]}`) {
+        diagonalUpperleft++;
+      }
+      if (rows.item(i).children.item(2 - i).innerHTML === `${this.#players[this.#turn]}`) {
+        diagonalUpperRight++;
+      }
+    }
+    if (diagonalUpperleft === 3 || diagonalUpperRight === 3) { return true; }
+    
+    let rowCount = 0;
+    let columnCount = 0;
+    for (let i = 0; i < 3; ++i) {
+      for (let j = 0; j < 3; ++j) {
+        if (rows.item(i).children.item(j).innerHTML === `${this.#players[this.#turn]}`) { rowCount++; }
+        if (rows.item(j).children.item(i).innerHTML === `${this.#players[this.#turn]}`) { columnCount++; }
+        if (rowCount === 3 || columnCount === 3) { return true; }
+
+      }
+      rowCount = 0;
+      columnCount = 0;
+    }
+    return false;
+  }
+
+  #reset() {
+    const board = this.#elements.table.children;
+
+    for (let i = 0; i < 3; ++i) {
+      for (let j = 0; j < 3; ++j) {
+        board.item(i).children.item(j).innerHTML = ' ';
+      }
+    }
+
+    this.#gameOver = false;
+    this.#turn = 0;
+
+    this.#clearStatus();
+  }
+
+  #clearStatus() {
+    this.#elements.status.innerHTML = "";
+  }
+}
+
+function PlayTicTacToe() {
+  const container = document.querySelector(".tictactoe-container");
+  const button = document.querySelector(".tictactoe-btn");
+
+  new TicTacToe(container, button);
+}
+
+PlayTicTacToe();
+
