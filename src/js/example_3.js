623dc472e618c991bbf64be05d67b7ae5ebb91d3
codingsamplevc <150186422+codingsamplevc@users.noreply.github.com>
2026-03-26 21:41:44 +0100

    refactor: into class

--- src/js/example_3.js
+++ src/js/example_3.js
@@ -1,33 +1,54 @@
-
 // JavaScript example 3: Conway's 'Game of Life'
 
-const GoL = {
-  interval_id: null,
-  auto_btn_id: "auto_sim",
-  game_state: {},
-  height: 25,
-  width: 25,
-  get_Adjacent: function (x_idx, y_idx, x_idx_max, y_idx_max){
-    if (x_idx_max != 0 && y_idx_max != 0){
+class GameOfLife {
+  #height = 25;
+  #width = 25;
+  #autoSimOn = false;
+  #classNames = {
+    container: "gameoflife-container",
+    description: "gameoflife-description",
+    board: "gameoflife-board",
+    advanceBtn: "gameoflife-adv-btn",
+    autoBtn: "gameoflife-auto-btn",
+    autoTimer: "gameoflife-auto-timer"
+  };
+  #elements = {
+    container: null,
+    board: null,
+    advanceBtn: null,
+    autoBtn: null,
+    autoTimer: null
+  };
+  #gameState = {};
+
+  constructor() {
+    this.#elements.container = document.querySelector(`.${this.#classNames.container}`);
+    if (!(document.querySelector(`.${this.#classNames.board}`)) && this.#elements.container) {
+      this.#createBoard();
+    }
+  }
+
+  #getAdjacent(x_index, y_index, x_index_max, y_index_max) {
+    if (x_index_max != 0 && y_index_max != 0) {
       const adjacent = [];
-      const x = [x_idx];
-      const y = [y_idx];
-      if (x_idx > 0){
-        x.push(x_idx - 1);
+      const x = [x_index];
+      const y = [y_index];
+      if (x_index > 0){
+        x.push(x_index - 1);
       }
-      if (x_idx < x_idx_max){
-        x.push(x_idx + 1);
+      if (x_index < x_index_max){
+        x.push(x_index + 1);
       }
-      if (y_idx > 0){
-        y.push(y_idx - 1);
+      if (y_index > 0){
+        y.push(y_index - 1);
       }
-      if (y_idx < y_idx_max){
-        y.push(y_idx + 1);
+      if (y_index < y_index_max){
+        y.push(y_index + 1);
       }
   
       for (const x_coord of x){
         for (const y_coord of y){
-          if (x_coord != x_idx || y_coord != y_idx){
+          if (x_coord != x_index || y_coord != y_index){
             adjacent.push(`${x_coord}_${y_coord}`);
           }
         }
@@ -35,211 +56,105 @@ const GoL = {
   
       return adjacent;
     }
-  },
-  get_AdjacentLiving: function (adjacent){
+  }
+
+  #getAdjacentLiving (adjacent) {
     let adjacent_living = 0;
     for (const cell_id of adjacent){
-      if (GoL.game_state[cell_id]){
+      if (this.#gameState[cell_id]){
         adjacent_living++;
       }
     }
     return adjacent_living;
-  },
-  isAlive: function (currently_alive, adjacent_living){
+  }
+
+  #isAlive (currently_alive, adjacent_living) {
     if (currently_alive){ return (adjacent_living > 1 && adjacent_living < 4); }
     else { return adjacent_living == 3; }
-  },
-  advance: function (){
+  }
+
+  #advance() {
     const cellsToChange = [];
-    for (const cell in GoL.game_state){
+    for (const cell in this.#gameState){
       const cell_coords = cell.split('_');
-      if (GoL.game_state[cell] != GoL.isAlive(GoL.game_state[cell], GoL.get_AdjacentLiving(GoL.get_Adjacent(parseInt(cell_coords[0]), parseInt(cell_coords[1]), GoL.height - 1, GoL.width - 1)))){
+      if (this.#gameState[cell] != this.#isAlive(this.#gameState[cell], this.#getAdjacentLiving(this.#getAdjacent(parseInt(cell_coords[0]), parseInt(cell_coords[1]), this.#height - 1, this.#width - 1)))){
         cellsToChange.push(cell);
       }
     }
     for (const cell of cellsToChange){
-      GoL.game_state[cell] = !(GoL.game_state[cell]);
+      this.#gameState[cell] = !(this.#gameState[cell]);
       const cell_el = document.getElementById(cell);
-      if (cell_el.className == 'alive'){
-        cell_el.className = 'dead';
+      if (cell_el.className == "alive"){
+        cell_el.className = "dead";
       } else {
-        cell_el.className = 'alive';
+        cell_el.className = "alive";
       }
     }
-  },
-  flip: function (cell_id){
-    GoL.game_state[cell_id] = !(GoL.game_state[cell_id]);
+  }
+
+  #flip(cell_id) {
+    this.#gameState[cell_id] = !(this.#gameState[cell_id]);
     const cell = document.getElementById(cell_id);
     if (cell.className == "alive"){
       cell.className = "dead";
     } else {
       cell.className = "alive";
     }
-  },
-  handleCellClick: function (e){
-    GoL.flip(e.target.id);
-  },
-  stopAutoSimulation: function (){
-    clearInterval(GoL.interval_id);
-    GoL.interval_id = null;
-    const auto_btn = document.getElementById(GoL.auto_btn_id);
-    auto_btn.setAttribute("onclick", "GoL.startAutoSimulation()");
-  },
-  startAutoSimulation: function (){
-    if (!GoL.interval_id){
-      GoL.interval_id = setInterval(GoL.advance, 1500);
-      const auto_btn = document.getElementById(GoL.auto_btn_id);
-      auto_btn.setAttribute("onclick", "GoL.stopAutoSimulation()");
+  }
+  
+  #handleCellClick(e) {
+    this.#flip(e.target.id);
+  }
+
+  #autoSimulation() {
+    // if (!this.#elements.autoTimer) { return; }
+    this.#autoSimOn = !this.#autoSimOn;
+    if (this.#autoSimOn) {
+      this.#elements.autoTimer = setInterval(this.#advance, 1500);
+    } else {
+      clearInterval(this.#elements.autoTimer);
     }
-  },
-  createBoard: function createBoard(container_id){
-    if (document.getElementById("gol_board")){ return; }
-    
+  }
+
+  #createBoard() {
     const board = document.createElement("table");
-    board.id = "gol_board";
-    
-    for (let i = 0; i < GoL.height; i++){
+    this.#elements.board = board;
+    board.className = this.#classNames.board;
+
+    for (let i = 0; i < this.#height; ++i) {
       let row = document.createElement("tr");
-      for (let j = 0; j < GoL.width; j++){
+      for (let j = 0; j < this.#width; ++j) {
         let cell = document.createElement("td");
         cell.id = `${i}_${j}`;
         cell.className = "dead";
         row.appendChild(cell);
-        cell.addEventListener("click", GoL.handleCellClick);
-        GoL.game_state[`${i}_${j}`] = 0;
+        cell.addEventListener("click", this.#handleCellClick);
+        this.#gameState[`${i}_${j}`] = 0;
       }
       board.appendChild(row);
     }
-  
-    const gol_desc = document.createElement("p");
-    gol_desc.innerHTML = "Click on the board to flip cells. Click the 'Advance' button to simulate a turn/cycle. Keep clicking to see how your setup turns out.";
-    const container = document.getElementById(container_id);
-    container.appendChild(gol_desc);
-    container.appendChild(board);
-  
-    const advance_btn = document.createElement("button");
-    advance_btn.innerHTML = "Advance";
-    advance_btn.addEventListener("click", GoL.advance);
-    container.appendChild(advance_btn);
 
-    const auto_simulation_btn = document.createElement("button");
-    auto_simulation_btn.id = GoL.auto_btn_id;
-    auto_simulation_btn.innerHTML = "Start/stop automatic simulation";
-    auto_simulation_btn.setAttribute("onclick", "GoL.startAutoSimulation()");
-    container.appendChild(auto_simulation_btn);
+    const description = document.createElement("p");
+    description.innerText = "Click on the board to flip cells. Click the 'Advance' button to simulate a turn/cycle. Keep clicking to see how your setup turns out.";
+
+    this.#elements.container.appendChild(description);
+    this.#elements.container.appendChild(board);
+
+    const advanceBtn = document.createElement("button");
+    this.#elements.advanceBtn = advanceBtn;
+    advanceBtn.innerText = "Advance";
+    advanceBtn.addEventListener("click", this.#advance);
+
+    this.#elements.container.appendChild(advanceBtn);
+
+    const autoBtn = document.createElement("button");
+    this.#elements.autoBtn = autoBtn;
+    autoBtn.className = this.#classNames.autoBtn;
+    autoBtn.innerText = "Start/stop automatic simulation";
+    autoBtn.addEventListener("click", this.#autoSimulation);
+
+    this.#elements.container.appendChild(autoBtn);
   }
-};
+}
 
-// const game_state = {};
-// const height = 25;
-// const width = 25;
-
-// function getAdjacent(x_idx, y_idx, x_idx_max, y_idx_max){
-//   if (x_idx_max != 0 && y_idx_max != 0){
-//     const adjacent = [];
-//     const x = [x_idx];
-//     const y = [y_idx];
-//     if (x_idx > 0){
-//       x.push(x_idx - 1);
-//     }
-//     if (x_idx < x_idx_max){
-//       x.push(x_idx + 1);
-//     }
-//     if (y_idx > 0){
-//       y.push(y_idx - 1);
-//     }
-//     if (y_idx < y_idx_max){
-//       y.push(y_idx + 1);
-//     }
-
-//     for (const x_coord of x){
-//       for (const y_coord of y){
-//         if (x_coord != x_idx || y_coord != y_idx){
-//           adjacent.push(`${x_coord}_${y_coord}`);
-//         }
-//       }
-//     }
-
-//     return adjacent;
-//   }
-// }
-
-// function checkAdjacent(adjacent){
-//   let adjacent_living = 0;
-//   for (const cell_id of adjacent){
-//     if (game_state[cell_id]){
-//       adjacent_living++;
-//     }
-//   }
-//   return adjacent_living;
-// }
-
-// function isAlive(currently_alive, adjacent_number){
-//   if (currently_alive){ return (adjacent_number > 1 && adjacent_number < 4); }
-//   else { return adjacent_number == 3; }
-// }
-
-// function advanceGOL(){
-//   const cellsToChange = [];
-//   for (const cell in game_state){
-//     const cell_coords = cell.split('_');
-//     if (game_state[cell] != isAlive(game_state[cell], checkAdjacent(getAdjacent(parseInt(cell_coords[0]), parseInt(cell_coords[1]), height - 1, width - 1)))){
-//       cellsToChange.push(cell);
-//     }
-//   }
-//   for (const cell of cellsToChange){
-//     game_state[cell] = !(game_state[cell]);
-//     const cell_el = document.getElementById(cell);
-//     if (cell_el.className == 'alive'){
-//       cell_el.className = 'dead';
-//     } else {
-//       cell_el.className = 'alive';
-//     }
-//   }
-// }
-
-// function changeCell(cell_id){
-//   game_state[cell_id] = !(game_state[cell_id]);
-//   const cell = document.getElementById(cell_id);
-//   if (cell.className == "alive"){
-//     cell.className = "dead";
-//   } else {
-//     cell.className = "alive";
-//   }
-// }
-
-// function handleCellClick(e){
-//   console.log("clicked " + e.target.id);
-//   changeCell(e.target.id);
-// }
-
-// function createBoard(el_id){
-//   if (document.getElementById("gol_board")){ return; }
-  
-//   const board = document.createElement("table");
-//   board.id = "gol_board";
-  
-//   for (let i = 0; i < height; i++){
-//     let row = document.createElement("tr");
-//     for (let j = 0; j < width; j++){
-//       let cell = document.createElement("td");
-//       cell.id = `${i}_${j}`;
-//       cell.className = "dead";
-//       row.appendChild(cell);
-//       cell.addEventListener("click", handleCellClick);
-//       game_state[`${i}_${j}`] = 0;
-//     }
-//     board.appendChild(row);
-//   }
-
-//   const gol_desc = document.createElement("p");
-//   gol_desc.innerHTML = "Click on the board to flip cells. Click the 'Advance' button to simulate a turn/cycle. Keep clicking to see how your setup turns out.";
-//   const container = document.getElementById(el_id);
-//   container.appendChild(gol_desc);
-//   container.appendChild(board);
-
-//   const advance_btn = document.createElement("button");
-//   advance_btn.innerHTML = "Advance";
-//   advance_btn.addEventListener("click", advanceGOL);
-//   container.appendChild(advance_btn);
-// }
+GameOfLife();
